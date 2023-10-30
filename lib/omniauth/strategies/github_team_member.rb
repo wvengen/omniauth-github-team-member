21f4ebc3f227a89d315bf33426a096c250bd3a0d
wvengen <willem@thequestionmark.org>
2023-10-30 17:53:51 +0100

    Allow looking up teams by slug, remove email
    Emails are present in the Github strategy already.
    Pending: updating README.

--- lib/omniauth/strategies/github_team_member.rb
+++ lib/omniauth/strategies/github_team_member.rb
@@ -4,26 +4,37 @@ module OmniAuth
   module Strategies
     class GitHubTeamMember < OmniAuth::Strategies::GitHub
       credentials do
-        options['teams'].inject({}) do |base, key_value_pair|
-          method_name, team_id = key_value_pair
-          base[booleanize_method_name(method_name)] = team_member?(team_id)
-          base["email"] = email_addresses
-          base
+        if team_access_allowed?
+          options['teams'].inject({}) do |base, (method_name, org_team_slug)|
+            # old method is to specify a team_id, using a deprecated endpoint;
+            # new method is to specify organisation and team slug
+            org, team_slug = org_team_slug.split('/', 2)
+            membership = team_slug ? team_member_new?(org, team_slug) : team_member_old(org)
+            base[booleanize_method_name(method_name)] = membership
+            base
+          end
+        else
+          {}
         end
       end
 
-      def email_addresses
-        response = access_token.get("/user/emails")
-        response.status == 200 && response.parsed.map{|user| user["email"]}
-      rescue ::OAuth2::Error
-        false
+      def team_member_new?(org, team_slug)
+        access_token.options[:mode] = :header
+        response = access_token.get("/orgs/#{org}/teams/#{team_slug}/memberships/#{raw_info['login']}", :headers => { 'Accept' => 'application/vnd.github.v3', 'X-GitHub-Api-Version' => '2022-11-28' })
+        response.status == 200 && response.parsed["state"] == "active"
       end
 
-      def team_member?(team_id)
-        response = access_token.get("/teams/#{team_id}/memberships/#{raw_info['login']}")
+      def team_member_old?(team_id)
+        access_token.options[:mode] = :header
+        response = access_token.get("/teams/#{team_id}/memberships/#{raw_info['login']}", :headers => { 'Accept' => 'application/vnd.github.v3', 'X-GitHub-Api-Version' => '2022-11-28' })
         response.status == 200 && response.parsed["state"] == "active"
-      rescue ::OAuth2::Error
-        false
+      end
+
+      def team_access_allowed?
+        return false unless options['scope']
+        team_scopes = ['org', 'read:org', 'write:org', 'admin:org']
+        scopes = options['scope'].split(',')
+        (scopes & team_scopes).any?
       end
 
       def booleanize_method_name(method_name)
